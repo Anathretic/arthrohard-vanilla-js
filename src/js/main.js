352370296f84568df9abd3893f41af5ea9a8d070
Konrad <konrad.wojtylo.9@gmail.com>
2024-08-02 00:34:02 +0200

    Added a fully functional product section

--- src/js/main.js
+++ src/js/main.js
@@ -34,3 +34,84 @@ const navHeight = document.querySelector('.nav__desktop').offsetHeight;
 document.documentElement.style.setProperty('--scroll-padding', navHeight - 1 + 'px');
 
 window.addEventListener('scroll', handleScrollSpy);
+
+//---------------------------------------------------------------------------------------------
+
+const URL = 'https://brandstestowy.smallhost.pl/api/';
+let productArrayValue = 8;
+let isVisible = false;
+let selectedItem = null;
+
+const productsContainer = document.getElementById('productsContainer');
+const productSelect = document.getElementById('productSelect');
+const errorElement = document.getElementById('error');
+const popupContainer = document.getElementById('popupContainer');
+const popupContent = document.getElementById('popupContent');
+const popupId = document.getElementById('popupId');
+const popupName = document.getElementById('popupName');
+const popupValue = document.getElementById('popupValue');
+const closePopupButton = document.getElementById('closePopup');
+
+productSelect.addEventListener('change', e => {
+	productArrayValue = Number(e.currentTarget.value);
+	fetchProducts();
+});
+
+closePopupButton.addEventListener('click', () => {
+	popupContainer.style.display = 'none';
+});
+
+const onScroll = () => {
+	const scrolledTo = window.scrollY + window.innerHeight;
+	if (document.body.scrollHeight === scrolledTo) {
+		productArrayValue += 12;
+		fetchProducts();
+	}
+};
+
+window.addEventListener('scroll', onScroll);
+
+const observer = new IntersectionObserver(
+	entries => {
+		if (entries[0].isIntersecting && !isVisible) {
+			isVisible = true;
+			observer.disconnect();
+			fetchProducts();
+		}
+	},
+	{
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.1,
+	}
+);
+
+observer.observe(document.getElementById('produkty'));
+
+const fetchProducts = () => {
+	fetch(`${URL}random?pageNumber=1&pageSize=${productArrayValue}`)
+		.then(response => response.json())
+		.then(data => {
+			if (data && data.data) {
+				productsContainer.innerHTML = '';
+				data.data.forEach(item => {
+					const productElement = document.createElement('div');
+					productElement.className = 'product__item';
+					productElement.innerHTML = `<p class='product__item-text'>ID: ${item.id}</p>`;
+					productElement.addEventListener('click', () => handleClick(item));
+					productsContainer.appendChild(productElement);
+				});
+			}
+		})
+		.catch(() => {
+			errorElement.style.display = 'block';
+		});
+};
+
+const handleClick = item => {
+	selectedItem = item;
+	popupId.textContent = `ID ${item.id}`;
+	popupName.textContent = item.name;
+	popupValue.textContent = item.value;
+	popupContainer.style.display = 'flex';
+};
